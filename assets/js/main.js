149a39eb263490ad5421116939feaeacf2297a77
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2022-11-05 20:20:17 +0400

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,12 @@ $(document).ready(function () {
   $(".swiper-button-next").on("click", () => swiper.slideNext());
 
   $(".swiper-button-prev").on("click", () => swiper.slidePrev());
+  $('.table-head__mobile .table-head').on('click',function (){
+    const $this = $(this),
+        element = $this.data('element')
+    $this.parent().children().removeClass('active')
+    $this.addClass('active')
+    $('.section-option__table').find('td').hide()
+    $('.section-option__table').find(element).show()
+  })
 })
